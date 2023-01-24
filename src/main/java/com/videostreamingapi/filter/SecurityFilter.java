50bc5ae0b6986531590af107d5a6e571f548d158
MykhailoDm <mikhailo2002dm@gmail.com>
2023-01-24 22:53:51 +0200

    Implemented SecurityFilter to retrieve authentication

--- src/main/java/com/videostreamingapi/filter/SecurityFilter.java
+++ src/main/java/com/videostreamingapi/filter/SecurityFilter.java
@@ -1,19 +1,67 @@
 package com.videostreamingapi.filter;
 
+import com.videostreamingapi.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.swing.text.html.Option;
 import java.io.IOException;
+import java.util.Optional;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
+    public SecurityFilter(
+            JwtService jwtService,
+            @Qualifier("UserDetailsServiceImpl") UserDetailsService userDetailsService
+    ) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // TODO implement
+        try {
+            Optional<String> jwtOptional = retrieveJwtFrom(request);
+            if (jwtOptional.isPresent() && jwtService.validateJwtToken(jwtOptional.get())) {
+                final String jwt = jwtOptional.get();
+                String username = jwtService.getUsernameFromJwtToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> retrieveJwtFrom(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return Optional.of(headerAuth.substring(7));
+        }
+
+        return Optional.empty();
     }
 }
